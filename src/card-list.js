f889884de89d4d4f0143728f27235ddd8d921bb2
rakib-anam <52469465+rakib-anam@users.noreply.github.com>
2023-03-03 21:04:31 -0500

    Deleted openWC template and Added all 5 pokemon cards working properly

--- src/card-list.js
+++ src/card-list.js
@@ -2,8 +2,6 @@ import { LitElement, html, css } from 'lit';
 
 import "pokemon-card-in-open-wc/src/pokemon-card-in-open-wc.js";
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
-
 class CardList extends LitElement {
   static properties = {
     header: { type: String },
@@ -23,50 +21,44 @@ class CardList extends LitElement {
       text-align: center;
       background-color: var(--card-list-background-color);
     }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
   `;
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.header = 'Card List';
   }
 
   render() {
     return html`
-      <pokemon-card-in-open-wc name='Pikachu' imageUrl='https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg'
-      topText='Pika' bottomText='Pika'>
-      <p>
-        Pikachi is a mouse pokemon.
-      </p>
+      <pokemon-card-in-open-wc name='Charizard'
+      imageUrl='https://img2.cgtrader.com/items/4040895/1b0ac08b24/pokemon-charizard-easy-print-no-support-3d-model-stl.jpg'
+      topText='CHARIII' bottomText='ZAAARD'
+      >
+      <p>Charizard is a fire type pokemon</p>
       </pokemon-card-in-open-wc>
 
-      <pokemon-card-in-open-wc imageUrl='https://i.pinimg.com/originals/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg'>
+      <pokemon-card-in-open-wc name='Pikachu' 
+      imageUrl='https://mito3d.fra1.digitaloceanspaces.com/3Dprintmodels/cults/pikachu_pokemon5'
+      topText='PIKA' bottomText='PIKA'>
+      <p>Pikachu is a thunder type pokemon</p>
+      </pokemon-card-in-open-wc>
+
+      <pokemon-card-in-open-wc name='Snorlax' 
+      imageUrl='https://i.etsystatic.com/33901625/r/il/d3c51d/4226249756/il_fullxfull.4226249756_6sqb.jpg'
+      topText='YAAAWN' bottomText='YAAAWN'>
+      <p>Snorlax is a normal type pokemon</p>
+      </pokemon-card-in-open-wc>
+
+      <pokemon-card-in-open-wc name='Mew' 
+      imageUrl='https://cdn2.myminifactory.com/assets/object-assets/5fe544b1c6d0c/images/720X720-mew-2.jpg'
+      topText='MEW' bottomText='NEW'>
+      <p>Mew is a phychic type pokemon</p>
+      </pokemon-card-in-open-wc>
+
+      <pokemon-card-in-open-wc name='Mewtwo' 
+      imageUrl='https://img1.cgtrader.com/items/3182452/bcde20cd24/mewtwo-pokemon-3d-model-bcde20cd24.jpg'
+      topText='MEW' bottomText='TWO'>
+      <p>Mewtwo is a phychic type pokemon</p>
       </pokemon-card-in-open-wc>
     `;
   }
